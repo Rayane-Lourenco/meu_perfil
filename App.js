059e73320b8e5ff451d3b257ffddccc679b795e9
Rayane-Lourenco <rayane.lourenco@escola.pr.gov.br>
2025-11-03 09:52:38 -0300

    edit app.js

--- App.js
+++ App.js
@@ -1,20 +1,77 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {useEffect, useState} from "react";
+import { StyleSheet, Text, View, Image, ActivityIndicator, ScrollView } from "react-native";
 
 export default function App() {
+  const [perfil, setPerfil] = useState(null);
+  const [loading, setLoading] = useState(true);
+  
+  useEffect(() => {
+    fetch("http://192.168.56.1:3000/api/perfils")
+    .then((res) => res.json())
+    .then((json) => { 
+      console.log(json);
+      setPerfil(json);
+      setLoading(false);
+    })
+    .catch(err => {
+      console.log(err);
+      setLoading(false);
+    })
+  }, []);
+
+  if (loading) {
+    return (
+      <View style={styles.container}>
+        <ActivityIndicator size="large" color="#0000ff" />
+        <Text>Carregando...</Text>
+      </View>
+    );
+  }
+   
+  if (!perfil) {
+    console.log(perfil);
+    return (
+      <View style={styles.container}>
+        <Text>Erro ao carregar dados</Text>
+      </View>
+    );
+  }
+    
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <ScrollView contentContainerStyle={styles.container}>
+      <Image source={{uri: perfil.image}} style={styles.image} />
+      <Text style={styles.titulo}>Perfil</Text>
+      <Text style={styles.texto}>Nome:{perfil.nome}</Text>
+      <Text style={styles.texto}>Idade:{perfil.idade}</Text>
+    </ScrollView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: "center",
+    alignItems: "center",
+    flexGrow: 1,
+    padding: 20,
+    backgroundColor: "#fff",
+  },
+  titulo: {
+    fontSize: 24,
+    fontWeight: "bold",
+    marginTop: 20,
+    marginBottom: 10,
+    width: "100%",
+  },
+  texto: {
+    fontSize: 18,
+    marginBottom: 5,
+    width: "70%",
+    textAlign: "center",
+  },
+  image: {
+    width: 120,
+    height: 120,
+    borderRadius: 60,
+    marginBottom: 10,
   },
 });
